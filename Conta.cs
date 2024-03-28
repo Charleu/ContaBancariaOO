f3f704ba26a790fdf3bf399e6f3aa57a9248d562
Charleu Limeira <sdlimeira0@gmail.com>
2024-03-28 12:43:42 -0300

    Criando construtores e override

---
+++ Conta.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace ContaBancariaOO
+{
+    class Conta
+    {
+        public int NumeroConta { get; }
+        public string NomeTitular;
+        public double Saldo;
+
+        private static int proximoNumeroConta = 1000;
+
+        public Conta()
+        {
+            NumeroConta = proximoNumeroConta++;
+            Console.WriteLine("Digite o nome do titular da conta:");
+            NomeTitular = Console.ReadLine();
+
+            Console.WriteLine("Deseja realizar um depósito inicial? (s/n):");
+            string resposta = Console.ReadLine().ToLower();
+            double saldoInicial = 0;
+
+            if (resposta == "s")
+            {
+                Console.WriteLine("Digite o valor do depósito inicial:");
+                saldoInicial = double.Parse(Console.ReadLine());
+            }
+
+            if (saldoInicial > 0)
+                Saldo = saldoInicial;
+
+            ExibirInformacoes();
+            RealizarOperacao("depositar");
+            RealizarOperacao("sacar");
+        }
+
+        public static Conta CriarConta()
+        {
+            return new Conta();
+        }
+
+        public static Conta CriarConta(string nomeTitular)
+        {
+            return new Conta { NomeTitular = nomeTitular };
+        }
+
+        public static Conta CriarConta(string nomeTitular, double saldoInicial)
+        {
+            return new Conta { NomeTitular = nomeTitular, Saldo = saldoInicial };
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine($" Depositado com sucesso.\n Novo saldo: {Saldo.ToString("C")}");
+            Console.WriteLine("----------------------------------------");
+        }
+
+        public bool Sacar(double valor)
+        {
+            Saldo -= valor;
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine($" Saque de {valor.ToString("C")} realizado com sucesso.\n Novo saldo: {Saldo.ToString("C")}");
+            Console.WriteLine("----------------------------------------");
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}\nTitular: {NomeTitular}\nSaldo: {Saldo.ToString("C")}";
+        }
+
+        private void ExibirInformacoes()
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine(ToString());
+            Console.WriteLine("----------------------------------------");
+        }
+
+        private void RealizarOperacao(string operacao)
+        {
+            Console.WriteLine($"\nDigite o valor para {operacao}:");
+            double valor = double.Parse(Console.ReadLine());
+
+            if (operacao == "sacar")
+                Sacar(valor + 5.0); 
+            else
+                Depositar(valor);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,49 +4,9 @@ namespace ContaBancariaOO
 {
     class Program
     {
-        static int numeroConta = 1000;
-
         static void Main(string[] args)
         {
-            string nomeTitular;
-            double saldo = 0.0;
-            Console.WriteLine("Digite o nome do titular da conta:");
-            nomeTitular = Console.ReadLine();
-            Console.WriteLine("Deseja realizar um depósito inicial? (s/n):");
-            if (Console.ReadLine().ToLower() == "s")
-            {
-                Console.WriteLine("Digite o valor do depósito inicial:");
-                saldo += double.Parse(Console.ReadLine());
-            }
-
-            ExibirInformacoesConta(numeroConta, nomeTitular, saldo);
-            saldo = RealizarOperacao("depositar", saldo);
-            saldo = RealizarOperacao("sacar", saldo);
-
-            numeroConta++;
-        }
-
-        public static double RealizarOperacao(string operacao, double saldo)
-        {
-            Console.WriteLine($"\nDigite o valor para {operacao}:");
-            double valor = double.Parse(Console.ReadLine());
-
-            if (operacao == "sacar")
-                saldo -= valor + 5.0;
-            else
-                saldo += valor;
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine($"Novo saldo após {operacao}: {saldo.ToString("C")}");
-            Console.WriteLine("----------------------------------------");
-
-            return saldo;
-        }
-
-        public static void ExibirInformacoesConta(int numeroConta, string nomeTitular, double saldo)
-        {
-            Console.WriteLine("----------------------------------------"); 
-            Console.WriteLine($" Conta {numeroConta}\n Titular: {nomeTitular}\n Saldo: {saldo.ToString("C")}");
-            Console.WriteLine("----------------------------------------");
+            Conta conta = Conta.CriarConta();
         }
     }
 }
